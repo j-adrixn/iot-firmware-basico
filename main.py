2cbfc3a2beef07b883779f17843b45e522239ab9
Adrian <j_adrian.15@hotmail.com>
2026-04-29 10:38:05 -0500

    Commit Inicial

---
+++ main.py
@@ -0,0 +1,11 @@
+import machine
+import time
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+print("¡Hola Mundo! Iniciando sistema IoT en mi cumple 29...")
+
+while True:
+    led.value(not led.value())
+    print("ESP32 volando con antigravity...")
+    time.sleep(1)
